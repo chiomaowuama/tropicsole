71781060095091aa433cc4aa64865f3d5ef3c66c
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2023-08-10 15:20:56 +0100

    third commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import navigations from '../components/navigations.vue'
+import footerpage from '../components/footerpage.vue'
 </script>
 
 <template>
+  <navigations/>
   <main>
     <h1>hello</h1>
   </main>
+  <footerpage/>
 </template>
